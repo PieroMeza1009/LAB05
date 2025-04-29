27c3655b22b1079ee513e8650ddcb1f8fc2890b9
PieroMeza10 <pieroms0905@gmail.com>
2025-04-29 16:22:30 -0500

    T2

--- src/actividades_L05/Tarea.java
+++ src/actividades_L05/Tarea.java
@@ -3,5 +3,11 @@ package actividades_L05;
 public class Tarea {
     private String titulo;
     private int prioridad;
+
+    public Tarea(String tarea, int prioridad){
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+
+    }
     
 }
