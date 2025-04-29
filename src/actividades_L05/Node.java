3bc76bf957cb550c94bc2706a62930c3a80ed030
PieroMeza10 <pieroms0905@gmail.com>
2025-04-29 16:38:26 -0500

    Nodo2

--- src/actividades_L05/Node.java
+++ src/actividades_L05/Node.java
@@ -4,6 +4,10 @@ package actividades_L05;
 public class Node<T> {
     private T data;
     private Node<T> siguiente;
-    
+
+    public Node(T data, Node<T> siguiente){
+        this.data = data;
+        this.siguiente = siguiente;
+    }
 
 }
