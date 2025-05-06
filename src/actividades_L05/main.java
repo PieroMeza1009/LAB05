8d49cc95fd354ee382e4a12244454b196dc63624
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 09:54:46 -0500

    M2

--- src/actividades_L05/main.java
+++ src/actividades_L05/main.java
@@ -4,6 +4,11 @@ public class main {
     public static void main(String[] args) {
         GestorDeTareas<Tarea> pendientes = new GestorDeTareas<>();
         TareasCompletadas<Tarea> completadas = new TareasCompletadas<>();
+
+
+        pendientes.agregarTarea(new Tarea("Estudiar para el examen", 5));
+        pendientes.agregarTarea(new Tarea("Limpiar el cuarto", 2));
+        pendientes.agregarTarea(new Tarea("Hacer tarea de AED", 4));
     }
 
 }
