01f2a0b89a202518ee9f1c4334e1e176405b9f65
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 13:04:04 -0500

    ME14

--- src/ejercicios_L05/MainEjercicios.java
+++ src/ejercicios_L05/MainEjercicios.java
@@ -35,6 +35,8 @@ public class MainEjercicios {
 
         /// 6. Concatenar dos listas
         Node<String> listaX = new Node<>("Hola", null);
+        Node<String> listaY = new Node<>("Mundo", null);
+        Node<String> listaConcat = UtilidadesListas.concatenarListas(listaX, listaY);
         
 
 
