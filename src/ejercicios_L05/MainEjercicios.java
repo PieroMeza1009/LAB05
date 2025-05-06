47f7b196e5d7407ecabee78398e121b77f4dcc9b
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 12:53:14 -0500

    ME2

--- src/ejercicios_L05/MainEjercicios.java
+++ src/ejercicios_L05/MainEjercicios.java
@@ -4,6 +4,11 @@ public class MainEjercicios {
     public static void main(String[] args) {
         // Crear lista de String
         Node<String> listaStr = new Node<>("A", new Node<>("B", new Node<>("C", null)));
+
+        // 1. Buscar un elemento
+        System.out.println("¿Contiene 'B'? " + Los_6Metodos.buscarElemento(listaStr, "B"));
+        System.out.println("¿Contiene 'Z'? " + Los_6Metodos.buscarElemento(listaStr, "Z"));
+        
     
     }
 
