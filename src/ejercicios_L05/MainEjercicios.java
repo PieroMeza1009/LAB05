7db9fb0f7c6d541fb154b1c14e0328c354e0fc52
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 12:52:01 -0500

    ME1

---
+++ src/ejercicios_L05/MainEjercicios.java
@@ -0,0 +1,10 @@
+package ejercicios_L05;
+
+public class MainEjercicios {
+    public static void main(String[] args) {
+        // Crear lista de String
+        Node<String> listaStr = new Node<>("A", new Node<>("B", new Node<>("C", null)));
+    
+    }
+
+}
