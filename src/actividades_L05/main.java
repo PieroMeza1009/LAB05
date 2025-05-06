f522d29b3160fa56ec2e4ad56a867310592a729d
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 09:53:31 -0500

    M1

---
+++ src/actividades_L05/main.java
@@ -0,0 +1,9 @@
+package actividades_L05;
+
+public class main {
+    public static void main(String[] args) {
+        GestorDeTareas<Tarea> pendientes = new GestorDeTareas<>();
+        TareasCompletadas<Tarea> completadas = new TareasCompletadas<>();
+    }
+
+}
