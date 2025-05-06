877d1b5801d0a1c44a71ce70c4ac668ab0d2f724
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 13:30:19 -0500

    cc1

--- src/ejercicios_L05/Los_6Metodos.java
+++ src/ejercicios_L05/Los_6Metodos.java
@@ -6,7 +6,8 @@ public class Los_6Metodos {
 
     //1. Buscar un elemento genérico en una lista
     public static <T> boolean buscarElemento(Node<T> cabeza, T valor) {
-        Node<T> actual = cabeza;
+        Node<T> actual = cabeza;// Comienza desde el nodo cabeza
+        // Mientras no se llegue al final de la lista
         while (actual != null) {
             if (actual.getData().equals(valor)) return true;
             actual = actual.getSiguiente();
