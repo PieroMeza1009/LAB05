1513cae77660b82ac76bc6dca6321159d2aced7a
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 13:10:09 -0500

    ME17

--- src/ejercicios_L05/MainEjercicios.java
+++ src/ejercicios_L05/MainEjercicios.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class MainEjercicios {
     public static void main(String[] args) {
         // Crear lista de String
-        Node<String> listaStr = new Node<>("A", new Node<>("B", new Node<>("C", null)))
+        Node<String> listaStr = new Node<>("A", new Node<>("B", new Node<>("C", null)));
 
         // 1. Buscar un elemento
         System.out.println("¿Contiene 'B'? " + Los_6Metodos.buscarElemento(listaStr, "B"));
         System.out.println("¿Contiene 'Z'? " + Los_6Metodos.buscarElemento(listaStr, "Z"));
 
         // 2. Invertir una lista
-        Node<String> invertida = Los_6Metodos.invertirLista(listaStr);
+        Node<String> listaInvertida = Los_6Metodos.invertirLista(listaStr);
         System.out.print("Lista invertida: ");
-        Los_6Metodos.imprimirLista(invertida);
+        Los_6Metodos.imprimirLista(listaInvertida);
 
         // 3. Insertar al final
         Node<Integer> listaNumeros = new Node<>(1, new Node<>(2, null));
