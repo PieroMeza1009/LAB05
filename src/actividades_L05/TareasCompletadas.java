2d493c6c0d66e9cf4043e6d1d9f3b4e543e400e4
PieroMeza10 <pieroms0905@gmail.com>
2025-05-05 23:21:28 -0500

    TCc2

--- src/actividades_L05/TareasCompletadas.java
+++ src/actividades_L05/TareasCompletadas.java
@@ -6,7 +6,7 @@ public class TareasCompletadas<T> {
 
         // aca el metodo para agregar una tarea completada al inicio de la lista
     public void agregarTareaCompletada(T tarea) {
-        // Se agrega al principio de la lista para facilidad            
+        // lo agrega al principio de la lista para facilidad            
         Node<T> nueva = new Node<>(tarea, cabeza);
         cabeza = nueva;
     }
@@ -15,12 +15,12 @@ public class TareasCompletadas<T> {
         Node<T> actual = cabeza;
         System.out.println("Tareas completadas:");
 
-         // Recorremos e imprimimos
+         //aca recorremos e imprimimos
         while (actual != null) {
             System.out.println(actual.getData());
             actual = actual.getSiguiente();
         }
-    // Método que devuelve cuántas tareas fueron completadas
+    // por ultimo un metoodo que devuelve cuántas tareas fueron completadas
     public int contarTareasCompletadas() {
         int contador = 0;
         Node<T> actual = cabeza;
