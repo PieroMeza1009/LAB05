822211dfc2b4f701715d39c2c2200252e8c6221f
PieroMeza10 <pieroms0905@gmail.com>
2025-05-06 10:53:30 -0500

    Mc3

--- src/actividades_L05/main.java
+++ src/actividades_L05/main.java
@@ -9,18 +9,22 @@ public class main {
         TareasCompletadas<Tarea> completadas = new TareasCompletadas<>();
 
         // Aca agregamos  tareas pendientes al gestor
-        pendientes.agregarTarea(new Tarea("Estudiar para el examen", 3));
-        pendientes.agregarTarea(new Tarea("Limpiar el cuarto", 1));
+        pendientes.agregarTarea(new Tarea("Estudiar para el examen", 1)); ///mientras menos sea el numero, mas alta la priodirdad
+        pendientes.agregarTarea(new Tarea("Limpiar el cuarto", 3));
         pendientes.agregarTarea(new Tarea("Hacer tarea de AED", 2));
 
+        //imprimimos todas las tereas pendientes
         System.out.println("Tareas pendientes:");
         pendientes.imprimirTareas();
 
-        Tarea tareaTerminada = new Tarea("Limpiar el cuarto", 2);
+        //aca creamos una tarea igual a la que ya existe para buscarla y marcarla como completada
+        Tarea tareaTerminada = new Tarea("Limpiar el cuarto", 3);
+        //si si se elimino la tarea, la agregamos a las ya completadas
         if (pendientes.eliminarTarea(tareaTerminada)) {
             completadas.agregarTareaCompletada(tareaTerminada);
         }
 
+        // Mostramos el estado de las listas después de completar una tarea
         System.out.println("\nDespués de completar una tarea:");
         System.out.println("Tareas pendientes:");
         pendientes.imprimirTareas();
@@ -28,10 +32,12 @@ public class main {
         System.out.println("\nTareas completadas:");
         completadas.imprimirTareasCompletadas();
 
+        // Obtenenemos y mostramos la tarea más prioritaria
         System.out.println("\nTarea más prioritaria:");
         Tarea masImportante = pendientes.obtenerTareaMasPrioritaria();
         System.out.println(masImportante);
 
+        //aca solo invertimos las tares
         pendientes.invertirTareas();
         System.out.println("\nTareas pendientes invertidas:");
         pendientes.imprimirTareas();
