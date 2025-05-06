683586a7f30420d37ff693dad4e2de4958dc8699
PieroMeza10 <pieroms0905@gmail.com>
2025-05-05 21:16:43 -0500

    G1

--- src/actividades_L05/GestordeTareas.java
+++ src/actividades_L05/GestordeTareas.java
@@ -1,5 +1,10 @@
 package actividades_L05;
 
 public class GestordeTareas {
+    private Node<T> cabeza;
+
+    public void agregarTarea(T tarea) {
+        Node<T> nuevo = new Node<>(tarea, null);
+    }
 
 }
